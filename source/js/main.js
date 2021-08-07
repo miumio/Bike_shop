16d6eacce60ef11b5e39193308e6cd17ac61d419
miumio <nickolskaya@gmail.com>
2021-08-07 22:59:31 +0300

    fix nav

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
-var nav = document.querySelector('.nav');
-var toggler = document.querySelector('.nav__toggler');
+const nav = document.querySelector('.nav');
+const toggler = document.querySelector('.nav__toggler');
 
 nav.classList.remove('nav--nojs');
 nav.classList.add('nav--closed');
@@ -14,4 +14,27 @@ toggler.addEventListener('click', function () {
     nav.classList.add('nav--closed');
     nav.classList.remove('nav--opened');
   }
+});
+
+const links = document.querySelectorAll('a[href*="#"]');
+const animationTime = 300;
+const framesCount = 20;
+
+links.forEach(function (item) {
+  item.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+    let scroller = setInterval(function () {
+      let scrollBy = coordY / framesCount;
+
+      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        window.scrollBy(0, scrollBy);
+      } else {
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    }, animationTime / framesCount);
+  });
 });
